AlexMax/odamex-alexmax

commit f29cd8c34c784bcd4683e2943e2a4ed3d61a1968

- Fix bug 529
git-svn-id: http://odamex.net/svn/root/trunk@1216 b0bcef46-2027-0410-bd56-ab726e9a1d04

common/d_dehacked.cpp
@@ -1503,6 +1503,7 @@ static int PatchText (int oldSize)
 			while (info->level_name) {
 				if (!strnicmp (info->music, temp, 8)) {
 					good = true;
+					sprintf (temp, "d_%s", newStr);
 					strncpy (info->music, temp, 8);
 				}
 				info++;

commit 06318c7b56d41a0f15fddb00b4b7d96e6b922e23

- Fix memory corruption introduced in r1216 that caused PL2 to crash
git-svn-id: http://odamex.net/svn/root/trunk@1251 b0bcef46-2027-0410-bd56-ab726e9a1d04

common/d_dehacked.cpp
@@ -1502,21 +1502,25 @@ static int PatchText (int oldSize)
 	// Search through music names.
 	// This is something of an even bigger hack
 	// since I changed the way music is handled.
-	if (oldSize < 7) {		// Music names are never >6 chars
-		if ( (temp = new char[oldSize + 3]) ) {
-			level_info_t *info = LevelInfos;
-			sprintf (temp, "d_%s", oldStr);
+	
+	// Music names are never >6 chars
+	if (oldSize < 7) 
+	{
+        level_info_t *info = LevelInfos;
+		char MusicLumpName[9] = { 0 };
+			
+		sprintf (MusicLumpName, "d_%s", oldStr);
 
-			while (info->level_name) {
-				if (!strnicmp (info->music, temp, 8)) {
-					good = true;
-					sprintf (temp, "d_%s", newStr);
-					strncpy (info->music, temp, 8);
-				}
-				info++;
+		while (info->level_name) 
+		{
+			if (!strnicmp (info->music, MusicLumpName, 8)) 
+			{
+				good = true;
+				sprintf (MusicLumpName, "d_%s", newStr);
+				strncpy (info->music, MusicLumpName, 8);
 			}
-
-			delete[] temp;
+		
+			info++;
 		}
 	}
 