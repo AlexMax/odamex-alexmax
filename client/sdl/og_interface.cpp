216a8e2608a6712b5e7856f7c398bd3033b592e1
AlexMax <alexmax2742@gmail.com>
2011-12-28 23:37:04 -0500

    Black rectangle fix and start of doom palette color scheme.

--- client/sdl/og_interface.cpp
+++ client/sdl/og_interface.cpp
@@ -35,15 +35,59 @@
 namespace cl {
 namespace odagui {
 
+// Default AGAR color scheme palette indexes.
+// FIXME: Come up with a different approach for this.  The AGAR docs are
+//        hideously out of date, and there are way more than 20-odd colors.
+// TODO: Unindent any non-placeholder colors
+// TODO: Assumes Doom palette, should probably support Heretic/Hexen/etc.
+const unsigned char colorscheme_default[LAST_COLOR] = {
+	0, // Global background
+	  9, // Standard container background
+	  23, // Lines (eg. in tables)
+	4, // Text
+	240, // Window background
+	  39, // Window highlight #1
+	  55, // Window highlight #2
+	  71, // Focused titlebar color
+	  87, // Unfocused titlebar color
+	4, // Titlebar text color
+	  103, // For button-like controls
+	  119, // For "disabled" controls
+	  135, // Non-selected menu item
+	  151, // Selected menu item
+	  167, // Boolean option for menu item
+	4, // Text of menu item
+	  183, // Menu separator #1
+	  199, // Menu separator #2
+	  215, // Text control background
+	  231, // Text control foreground
+	  251, // Text cursor
+	NULL, // Transparent color, ignored
+	21, // Movable pane separators
+	22, // Background of table widgets
+	23, // Lines of table widgets
+};
+
 // True if the GUI is visible in any capacity.  A visible GUI also means
 // that the event handler is running and has priority over Doom events.
-bool visible = false;
+bool visible = true;
 
 // SDL Surface that stores the GUI
 SDL_Surface *surface;
 
 // Color palette used for the surface
 SDL_Color colors[256];
+
+// Set AGAR color scheme based on doom palette indexes
+void set_colorscheme(const unsigned char cs[]) {
+	for (unsigned i = 0;i < LAST_COLOR;i++) {
+		if (i == TRANSPARENT_COLOR) {
+			continue;
+		}
+		size_t pi = cs[i];
+		AG_ColorsSetRGB(i, colors[pi].r, colors[pi].g, colors[pi].b);
+	}
+}
 
 // Set the correct palette for the GUI.
 void set_palette(DWORD *palette) {
@@ -53,6 +97,7 @@ void set_palette(DWORD *palette) {
 		colors[i].b = BPART(*palette);
 		colors[i].unused = 0;
 	}
+	set_colorscheme(colorscheme_default);
 }
 
 // Initialize canvas for a particular size
@@ -130,12 +175,14 @@ void draw() {
 
 	AG_BeginRendering(agDriverSw);
 	AG_FOREACH_WINDOW(win, agDriverSw) {
-		// Copy the rView to an SDL_Rect for future blitting.
-		SDL_Rect rect = {win->wid.rView.x1,
-						 win->wid.rView.y1,
-						 win->wid.rView.w,
-						 win->wid.rView.h};
-		rects.push_back(rect);
+		if (win->visible) {
+			// Copy the rView to an SDL_Rect for future blitting.
+			SDL_Rect rect = {win->wid.rView.x1,
+							 win->wid.rView.y1,
+							 win->wid.rView.w,
+							 win->wid.rView.h};
+			rects.push_back(rect);
+		}
 		// Render the window to the GUI surface.
 		AG_ObjectLock(win);
 		AG_WindowDraw(win);
