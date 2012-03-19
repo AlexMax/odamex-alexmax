AlexMax/odamex-alexmax

commit ec9d6f1cf8d90cfd8010744d0ee20318339a0f2a

-Restore zdoom sky2 sector effect, which will use the sky texture defined in the MAPINFO lump if present (or the normal sky if absent).
git-svn-id: http://odamex.net/svn/root/branches/0.6-dev@2829 b0bcef46-2027-0410-bd56-ab726e9a1d04

client/src/g_level.cpp
@@ -1012,7 +1012,9 @@ void G_DoLoadLevel (int position)
 	// depending on the current episode, and the game version.
 	// [RH] Fetch sky parameters from level_locals_t.
 	// [ML] 5/11/06 - remove sky2 remenants
+	// [SL] 2012-03-19 - Add sky2 back
 	sky1texture = R_TextureNumForName (level.skypic);
+    sky2texture = R_TextureNumForName (level.skypic2);
 
 	// [RH] Set up details about sky rendering
 	R_InitSkyMap ();
@@ -1190,8 +1192,10 @@ void G_InitLevelLocals ()
 		level_pwad_info_t *pinfo = wadlevelinfos + i;
 
 		// [ML] 5/11/06 - Remove sky scrolling and sky2
+		// [SL] 2012-03-19 - Add sky2 back
 		level.info = (level_info_t *)pinfo;
 		info = (level_info_t *)pinfo;
+		strncpy (level.skypic2, pinfo->skypic2, 8);
 		level.fadeto = pinfo->fadeto;
 		if (level.fadeto) {
 			NormalLight.maps = DefaultPalette->maps.colormaps;
@@ -1211,6 +1215,7 @@ void G_InitLevelLocals ()
 	} else {
 		info = FindDefLevelInfo (level.mapname);
 		level.info = info;
+		level.skypic2[0] = 0;
 		level.fadeto = 0;
 		level.outsidefog = 0xff000000;	// 0xff000000 signals not to handle it special
 		R_SetDefaultColormap ("COLORMAP");
@@ -1227,13 +1232,16 @@ void G_InitLevelLocals ()
 		strncpy (level.secretmap, info->secretmap, 8);
 		strncpy (level.music, info->music, 8);
 		strncpy (level.skypic, info->skypic, 8);
+		if (!level.skypic2[0])
+			strncpy(level.skypic2, level.skypic, 8);
 	} else {
 		level.partime = level.cluster = 0;
 		strcpy (level.level_name, "Unnamed");
 		level.nextmap[0] =
 			level.secretmap[0] =
 			level.music[0] = 0;
-			strncpy (level.skypic, "SKY1", 8);
+		strncpy (level.skypic, "SKY1", 8);
+		strncpy (level.skypic2, "SKY1", 8);
 		level.flags = 0;
 		level.levelnum = 1;
 	}

common/g_level.h
@@ -97,6 +97,7 @@ struct level_pwad_info_s
 	char		secretmap[9];
 	int			partime;
 	char		skypic[9];
+	char		skypic2[9];
 	char		music[9];
 	DWORD		flags;
 	int			cluster;
@@ -135,6 +136,7 @@ struct level_locals_s {
 
 	char		music[8];
 	char		skypic[8];
+	char		skypic2[8];
 
 	int			total_secrets;
 	int			found_secrets;

common/r_main.h
@@ -29,7 +29,7 @@
 #include "r_data.h"
 
 // killough 10/98: special mask indicates sky flat comes from sidedef
-#define PL_SKYFLAT (0x80000000)
+#define PL_SKYFLAT (0x8000)
 
 BOOL R_AlignFlat (int linenum, int side, int fc);
 

client/src/r_plane.cpp
@@ -561,9 +561,13 @@ void R_DrawPlanes (void)
 					skytex = sky1texture;
 					skyflip = 0;
 				}
+				else if (pl->picnum == PL_SKYFLAT)
+				{	// use sky2
+					skytex = sky2texture;
+					skyflip = 0;
+				}
 				else
 				{
-
 					// MBF's linedef-controlled skies
 					// Sky Linedef
 					short picnum = (pl->picnum & ~PL_SKYFLAT)-1;

common/r_sky.h
@@ -33,6 +33,7 @@ extern char SKYFLATNAME[8];
 extern int		sky1shift;				//		[ML] 5/11/06 - remove sky2 remenants
 
 extern int 		sky1texture;				//		""
+extern int 		sky2texture;				//		""
 extern fixed_t	skypos;					//		""
 extern fixed_t	skytexturemid;
 extern int		skystretch;

commit 1bfcca72ae1759bc6fc862a30d7bfd81da0446b6

-Fix a memory corruption issue introduced in r2829. -Fix a server compilation issue as well.
git-svn-id: http://odamex.net/svn/root/branches/0.6-dev@2831 b0bcef46-2027-0410-bd56-ab726e9a1d04

client/src/g_level.cpp
@@ -1016,7 +1016,7 @@ void G_DoLoadLevel (int position)
 	// [ML] 5/11/06 - remove sky2 remenants
 	// [SL] 2012-03-19 - Add sky2 back
 	sky1texture = R_TextureNumForName (level.skypic);
-    sky2texture = R_TextureNumForName (level.skypic2);
+	sky2texture = R_TextureNumForName (level.skypic2);
 
 	// [RH] Set up details about sky rendering
 	R_InitSkyMap ();

common/g_level.h
@@ -97,7 +97,6 @@ struct level_pwad_info_s
 	char		secretmap[9];
 	int			partime;
 	char		skypic[9];
-	char		skypic2[9];
 	char		music[9];
 	DWORD		flags;
 	int			cluster;
@@ -105,6 +104,7 @@ struct level_pwad_info_s
 	struct acsdefered_s *defered;
 
 	// level_pwad_info_s				[ML] 5/11/06 Removed sky scrolling/sky2
+	char		skypic2[9];
 	DWORD		fadeto;
 	char		fadetable[8];
 	DWORD		outsidefog;

common/r_data.cpp
@@ -948,6 +948,7 @@ void R_PrecacheLevel (void)
 	// [ML] 5/11/06 - Not anymore!
 
 	hitlist[sky1texture] = 1;
+	hitlist[sky2texture] = 1;
 
 	for (i = numtextures - 1; i >= 0; i--)
 	{

common/r_main.h
@@ -29,7 +29,7 @@
 #include "r_data.h"
 
 // killough 10/98: special mask indicates sky flat comes from sidedef
-#define PL_SKYFLAT (0x8000)
+#define PL_SKYFLAT (0x80000000)
 
 BOOL R_AlignFlat (int linenum, int side, int fc);
 

server/src/g_level.cpp
@@ -1076,6 +1076,7 @@ void G_DoLoadLevel (int position)
 	// depending on the current episode, and the game version.
 	// [RH] Fetch sky parameters from level_locals_t.
 	sky1texture = R_TextureNumForName (level.skypic);
+	sky2texture = R_TextureNumForName (level.skypic2);
 
 	for (i = 0; i < players.size(); i++)
 	{

server/src/r_sky.cpp
@@ -31,7 +31,7 @@
 
 // [ML] 5/11/06 - Remove sky2
 int 		skyflatnum;
-int 		sky1texture;
+int 		sky1texture, sky2texture;
 
 fixed_t		sky1pos=0,		sky1speed=0;
 