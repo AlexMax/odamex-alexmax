AlexMax/odamex-alexmax

commit 61ad8718176e105fc2a02761ea376596a792df15

-Add support for ZDoom line special type 181
git-svn-id: http://odamex.net/svn/root/branches/0.6-dev@2967 b0bcef46-2027-0410-bd56-ab726e9a1d04

client/src/cl_main.cpp
@@ -50,6 +50,7 @@
 #include "r_sky.h"
 #include "cl_demo.h"
 #include "cl_download.h"
+#include "p_local.h"
 #include "cl_maplist.h"
 #include "cl_vote.h"
 #include "p_mobj.h"
@@ -261,7 +262,6 @@ void P_ExplodeMissile (AActor* mo);
 void G_SetDefaultTurbo (void);
 void P_CalcHeight (player_t *player);
 bool P_CheckMissileSpawn (AActor* th);
-void P_SetFloorCeil(AActor *mo);
 void CL_SetMobjSpeedAndAngle(void);
 
 void P_PlayerLookUpDown (player_t *p);
@@ -943,6 +943,8 @@ void CL_MoveThing(AActor *mobj, fixed_t x, fixed_t y, fixed_t z)
 	mobj->z = z;
 	mobj->floorz = tmfloorz;
 	mobj->ceilingz = tmceilingz;
+	mobj->dropoffz = tmdropoffz;
+	mobj->floorsector = tmfloorsector;
 	mobj->LinkToWorld ();
 }
 
@@ -2251,8 +2253,9 @@ void CL_UpdateSector(void)
 		return;
 
 	sector_t *sec = &sectors[s];
-	sec->floorheight = fh << FRACBITS;
-	sec->ceilingheight = ch << FRACBITS;
+
+	P_SetFloorHeight(sec, fh << FRACBITS);
+	P_SetCeilingHeight(sec, ch << FRACBITS);
 
 	if(fp >= numflats)
 		fp = numflats;

common/p_doors.cpp
@@ -79,6 +79,9 @@ void DDoor::Serialize (FArchive &arc)
 //
 void DDoor::RunThink ()
 {
+	fixed_t ceilingheight = P_CeilingHeight(m_Sector);
+	fixed_t floorheight = P_FloorHeight(m_Sector);
+	
 	if (clientside && m_Status == destroy)
 	{
 		if (serverside)	// single player game
@@ -143,15 +146,11 @@ void DDoor::RunThink ()
 		
     case -1:
 		// DOWN
-        res = MoveCeiling (m_Speed, m_Sector->floorheight, false, m_Direction);
+        res = MoveCeiling (m_Speed, floorheight, false, m_Direction);
         if (m_Line && m_Line->id)
         {
             EV_LightTurnOnPartway(m_Line->id,
-                FixedDiv(
-                    m_Sector->ceilingheight - m_Sector->floorheight,
-                    m_TopHeight - m_Sector->floorheight
-                )
-            );
+                FixedDiv(ceilingheight - floorheight, m_TopHeight - floorheight));
         }
 		if (res == pastdest)
 		{
@@ -213,11 +212,7 @@ void DDoor::RunThink ()
         if (m_Line && m_Line->id)
         {
             EV_LightTurnOnPartway(m_Line->id,
-                FixedDiv(
-                    m_Sector->ceilingheight - m_Sector->floorheight,
-                    m_TopHeight - m_Sector->floorheight
-                )
-            );
+                FixedDiv(ceilingheight - floorheight, m_TopHeight - floorheight));
         }
 		if (res == pastdest)
 		{
@@ -332,6 +327,8 @@ DDoor::DDoor (sector_t *sec, line_t *ln, EVlDoor type, fixed_t speed, int delay)
 	memset(m_PlayedSound, false, sizeof(m_PlayedSound));
 	m_Status = init;
 
+	fixed_t ceilingheight = P_CeilingHeight(sec);
+	
 	switch (type)
 	{
 	case doorClose:
@@ -347,14 +344,14 @@ DDoor::DDoor (sector_t *sec, line_t *ln, EVlDoor type, fixed_t speed, int delay)
 		m_Direction = 1;
 		m_Status = opening;
 		m_TopHeight = P_FindLowestCeilingSurrounding (sec) - 4*FRACUNIT;
-		if (m_TopHeight != sec->ceilingheight)
+		if (m_TopHeight != ceilingheight)
 		{
 			PlayDoorSound();
 		}
 		break;
 
 	case doorCloseWaitOpen:
-		m_TopHeight = sec->ceilingheight;
+		m_TopHeight = ceilingheight;
 		m_Direction = -1;
 		m_Status = closing;
 		PlayDoorSound();

common/p_floor.cpp
@@ -289,12 +289,10 @@ BOOL EV_DoFloor (DFloor::EFloor floortype, line_t *line, int tag,
 				 fixed_t speed, fixed_t height, bool crush, int change)
 {
 	int 				secnum;
-	BOOL 				rtn;
+	BOOL 				rtn = false;
 	sector_t*			sec;
 	DFloor*				floor;
 	BOOL				manual = false;
-
-	rtn = false;
 
 	// check if a manual trigger; if so do just the sector on the backside
 	if (tag == 0)
@@ -316,6 +314,9 @@ manual_floor:
 		if (sec->floordata)
 			continue;
 
+		fixed_t floorheight = P_FloorHeight(sec);
+		fixed_t ceilingheight = P_CeilingHeight(sec);
+
 		// new floor thinker
 		rtn = true;
 		floor = new DFloor (sec);
@@ -323,7 +324,8 @@ manual_floor:
 		floor->m_Crush = false;
 		floor->m_Speed = speed;
 		floor->m_ResetCount = 0;				// [RH]
-		floor->m_OrgHeight = sec->floorheight;	// [RH]
+		floor->m_OrgHeight = floorheight;
+		
 
 		StartFloorSound (sec);
 
@@ -334,7 +336,7 @@ manual_floor:
 			floor->m_FloorDestHeight = P_FindHighestFloorSurrounding(sec);
 			// [RH] DOOM's turboLower type did this. I've just extended it
 			//		to be applicable to all LowerToHighest types.
-			if (floor->m_FloorDestHeight != sec->floorheight)
+			if (floor->m_FloorDestHeight != floorheight)
 				floor->m_FloorDestHeight += height;
 			break;
 
@@ -346,26 +348,25 @@ manual_floor:
 		case DFloor::floorLowerToNearest:
 			//jff 02/03/30 support lowering floor to next lowest floor
 			floor->m_Direction = -1;
-			floor->m_FloorDestHeight =
-				P_FindNextLowestFloor (sec, sec->floorheight);
+			floor->m_FloorDestHeight = P_FindNextLowestFloor(sec);
 			break;
 
 		case DFloor::floorLowerInstant:
 			floor->m_Speed = height;
 		case DFloor::floorLowerByValue:
 			floor->m_Direction = -1;
-			floor->m_FloorDestHeight = sec->floorheight - height;
+			floor->m_FloorDestHeight = floorheight - height;
 			break;
 
 		case DFloor::floorRaiseInstant:
 			floor->m_Speed = height;
 		case DFloor::floorRaiseByValue:
 			floor->m_Direction = 1;
-			floor->m_FloorDestHeight = sec->floorheight + height;
+			floor->m_FloorDestHeight = floorheight + height;
 			break;
 
 		case DFloor::floorMoveToValue:
-			floor->m_Direction = (height - sec->floorheight) > 0 ? 1 : -1;
+			floor->m_Direction = (height - floorheight) > 0 ? 1 : -1;
 			floor->m_FloorDestHeight = height;
 			break;
 
@@ -375,8 +376,8 @@ manual_floor:
 			floor->m_Direction = 1;
 			floor->m_FloorDestHeight =
 				P_FindLowestCeilingSurrounding(sec);
-			if (floor->m_FloorDestHeight > sec->ceilingheight)
-				floor->m_FloorDestHeight = sec->ceilingheight;
+			if (floor->m_FloorDestHeight > ceilingheight)
+				floor->m_FloorDestHeight = ceilingheight;
 			if (floortype == DFloor::floorRaiseAndCrush)
 				floor->m_FloorDestHeight -= 8 * FRACUNIT;
 			break;
@@ -388,7 +389,7 @@ manual_floor:
 
 		case DFloor::floorRaiseToNearest:
 			floor->m_Direction = 1;
-			floor->m_FloorDestHeight = P_FindNextHighestFloor(sec,sec->floorheight);
+			floor->m_FloorDestHeight = P_FindNextHighestFloor(sec);
 			break;
 
 		case DFloor::floorRaiseToLowest:
@@ -398,7 +399,7 @@ manual_floor:
 
 		case DFloor::floorRaiseToCeiling:
 			floor->m_Direction = 1;
-			floor->m_FloorDestHeight = sec->ceilingheight;
+			floor->m_FloorDestHeight = ceilingheight;
 			break;
 
 		case DFloor::floorLowerToLowestCeiling:
@@ -408,14 +409,14 @@ manual_floor:
 
 		case DFloor::floorLowerByTexture:
 			floor->m_Direction = -1;
-			floor->m_FloorDestHeight = sec->floorheight -
+			floor->m_FloorDestHeight = floorheight -
 				P_FindShortestTextureAround (secnum);
 			break;
 
 		case DFloor::floorLowerToCeiling:
 			// [RH] Essentially instantly raises the floor to the ceiling
 			floor->m_Direction = -1;
-			floor->m_FloorDestHeight = sec->ceilingheight;
+			floor->m_FloorDestHeight = ceilingheight;
 			break;
 
 		case DFloor::floorRaiseByTexture:
@@ -424,13 +425,13 @@ manual_floor:
 			//		since the code is identical to what was here. (Oddly
 			//		enough, BOOM preserved the code here even though it
 			//		also had this function.)
-			floor->m_FloorDestHeight = sec->floorheight +
+			floor->m_FloorDestHeight = floorheight +
 				P_FindShortestTextureAround (secnum);
 			break;
 
 		case DFloor::floorRaiseAndChange:
 			floor->m_Direction = 1;
-			floor->m_FloorDestHeight = sec->floorheight +	height;
+			floor->m_FloorDestHeight = floorheight + height;
 			sec->floorpic = line->frontsector->floorpic;
 			sec->special = line->frontsector->special;
 			break;
@@ -552,7 +553,7 @@ BOOL EV_DoChange (line_t *line, EChange changetype, int tag)
 			}
 			break;
 		case numChangeOnly:
-			secm = P_FindModelFloorSector (sec->floorheight,secnum);
+			secm = P_FindModelFloorSector(P_FloorHeight(sec), secnum);
 			if (secm) // if no model, no change
 			{
 				sec->floorpic = secm->floorpic;
@@ -627,13 +628,15 @@ manual_stair:
 				return rtn;
 		}
 
+		fixed_t floorheight = P_FloorHeight(sec);
+
 		// new floor thinker
 		rtn = true;
 		floor = new DFloor (sec);
 		floor->m_Direction = (type == DFloor::buildUp) ? 1 : -1;
 		floor->m_Type = DFloor::buildStair;	//jff 3/31/98 do not leave uninited
 		floor->m_ResetCount = reset;	// [RH] Tics until reset (0 if never)
-		floor->m_OrgHeight = sec->floorheight;	// [RH] Height to reset to
+		floor->m_OrgHeight = floorheight;	// [RH] Height to reset to
 		// [RH] Set up delay values
 		floor->m_Delay = delay;
 		floor->m_PauseTime = 0;
@@ -642,7 +645,7 @@ manual_stair:
 		floor->m_Crush = (!usespecials && speed == 4*FRACUNIT); //jff 2/27/98 fix uninitialized crush field
 
 		floor->m_Speed = speed;
-		height = sec->floorheight + stairsize * floor->m_Direction;
+		height = floorheight + stairsize * floor->m_Direction;
 		floor->m_FloorDestHeight = height;
 
 		texture = sec->floorpic;
@@ -737,7 +740,7 @@ manual_stair:
 				if (usespecials == 2)
 				{
 					// [RH]
-					fixed_t rise = (floor->m_FloorDestHeight - sec->floorheight)
+					fixed_t rise = (floor->m_FloorDestHeight - floorheight)
 									* floor->m_Direction;
 					floor->m_Speed = FixedDiv (FixedMul (speed, rise), stairsize);
 				}
@@ -749,7 +752,7 @@ manual_stair:
 				//jff 2/27/98 fix uninitialized crush field
 				floor->m_Crush = (!usespecials && speed == 4*FRACUNIT);
 				floor->m_ResetCount = reset;	// [RH] Tics until reset (0 if never)
-				floor->m_OrgHeight = sec->floorheight;	// [RH] Height to reset to
+				floor->m_OrgHeight = floorheight;	// [RH] Height to reset to
 			}
 		} while(ok);
 		if (manual)
@@ -801,7 +804,7 @@ int EV_DoDonut (int tag, fixed_t pillarspeed, fixed_t slimespeed)
 			floor->m_Speed = slimespeed;
 			floor->m_Texture = s3->floorpic;
 			floor->m_NewSpecial = 0;
-			floor->m_FloorDestHeight = s3->floorheight;
+			floor->m_FloorDestHeight = P_FloorHeight(s3);
 			floor->StartFloorSound ();
 
 			//	Spawn lowering donut-hole
@@ -811,7 +814,7 @@ int EV_DoDonut (int tag, fixed_t pillarspeed, fixed_t slimespeed)
 			floor->m_Direction = -1;
 			floor->m_Sector = s1;
 			floor->m_Speed = pillarspeed;
-			floor->m_FloorDestHeight = s3->floorheight;
+			floor->m_FloorDestHeight = P_FloorHeight(s3);
 			floor->StartFloorSound ();
 			break;
 		}
@@ -855,6 +858,9 @@ BOOL EV_DoElevator (line_t *line, DElevator::EElevator elevtype,
 		// If either floor or ceiling is already activated, skip it
 		if (sec->floordata || sec->ceilingdata) //jff 2/22/98
 			continue;
+		
+		fixed_t floorheight = P_FloorHeight(sec);
+		fixed_t ceilingheight = P_CeilingHeight(sec);
 
 		// create and initialize new elevator thinker
 		rtn = true;
@@ -871,42 +877,40 @@ BOOL EV_DoElevator (line_t *line, DElevator::EElevator elevtype,
 		// elevator down to next floor
 		case DElevator::elevateDown:
 			elevator->m_Direction = -1;
-			elevator->m_FloorDestHeight =
-				P_FindNextLowestFloor (sec, sec->floorheight);
+			elevator->m_FloorDestHeight = P_FindNextLowestFloor(sec);
 			elevator->m_CeilingDestHeight =
-				elevator->m_FloorDestHeight + sec->ceilingheight - sec->floorheight;
+				elevator->m_FloorDestHeight + ceilingheight - floorheight;
 			break;
 
 		// elevator up to next floor
 		case DElevator::elevateUp:
 			elevator->m_Direction = 1;
-			elevator->m_FloorDestHeight =
-				P_FindNextHighestFloor (sec, sec->floorheight);
+			elevator->m_FloorDestHeight = P_FindNextHighestFloor(sec);
 			elevator->m_CeilingDestHeight =
-				elevator->m_FloorDestHeight + sec->ceilingheight - sec->floorheight;
+				elevator->m_FloorDestHeight + ceilingheight - floorheight;
 			break;
 
 		// elevator to floor height of activating switch's front sector
 		case DElevator::elevateCurrent:
-			elevator->m_FloorDestHeight = line->frontsector->floorheight;
+			elevator->m_FloorDestHeight = P_FloorHeight(line->frontsector);
 			elevator->m_CeilingDestHeight =
-				elevator->m_FloorDestHeight + sec->ceilingheight - sec->floorheight;
+				elevator->m_FloorDestHeight + ceilingheight - floorheight;
 			elevator->m_Direction =
-				elevator->m_FloorDestHeight>sec->floorheight ? 1 : -1;
+				elevator->m_FloorDestHeight > floorheight ? 1 : -1;
 			break;
 
 		// [RH] elevate up by a specific amount
 		case DElevator::elevateRaise:
 			elevator->m_Direction = 1;
-			elevator->m_FloorDestHeight = sec->floorheight + height;
-			elevator->m_CeilingDestHeight = sec->ceilingheight + height;
+			elevator->m_FloorDestHeight = floorheight + height;
+			elevator->m_CeilingDestHeight = ceilingheight + height;
 			break;
 
 		// [RH] elevate down by a specific amount
 		case DElevator::elevateLower:
 			elevator->m_Direction = -1;
-			elevator->m_FloorDestHeight = sec->floorheight - height;
-			elevator->m_CeilingDestHeight = sec->ceilingheight - height;
+			elevator->m_FloorDestHeight = floorheight - height;
+			elevator->m_CeilingDestHeight = ceilingheight - height;
 			break;
 		}
 	}

common/p_plats.cpp
@@ -214,7 +214,7 @@ void DPlat::RunThink ()
 	case waiting:
 		if (!--m_Count)
 		{
-			if (m_Sector->floorheight == m_Low)
+			if (P_FloorHeight(m_Sector) == m_Low)
 				m_Status = up;
 			else
 				m_Status = down;
@@ -328,7 +328,7 @@ manual_plat:
 
 		//jff 1/26/98 Avoid raise plat bouncing a head off a ceiling and then
 		//going down forever -- default lower to plat height when triggered
-		plat->m_Low = sec->floorheight;
+		plat->m_Low = P_FloorHeight(sec);
 
 		if (change)
 		{
@@ -341,20 +341,20 @@ manual_plat:
 		switch (type)
 		{
 		case DPlat::platRaiseAndStay:
-			plat->m_High = P_FindNextHighestFloor (sec, sec->floorheight);
+			plat->m_High = P_FindNextHighestFloor(sec);
 			plat->m_Status = DPlat::midup;
 			plat->PlayPlatSound();
 			break;
 
 		case DPlat::platUpByValue:
 		case DPlat::platUpByValueStay:
-			plat->m_High = sec->floorheight + height;
+			plat->m_High = P_FloorHeight(sec) + height;
 			plat->m_Status = DPlat::midup;
 			plat->PlayPlatSound();
 			break;
 		
 		case DPlat::platDownByValue:
-			plat->m_Low = sec->floorheight - height;
+			plat->m_Low = P_FloorHeight(sec) - height;
 			plat->m_Status = DPlat::middown;
 			plat->PlayPlatSound();
 			break;
@@ -362,10 +362,10 @@ manual_plat:
 		case DPlat::platDownWaitUpStay:
 			plat->m_Low = P_FindLowestFloorSurrounding (sec) + lip*FRACUNIT;
 
-			if (plat->m_Low > sec->floorheight)
-				plat->m_Low = sec->floorheight;
+			if (plat->m_Low > P_FloorHeight(sec))
+				plat->m_Low = P_FloorHeight(sec);
 
-			plat->m_High = sec->floorheight;
+			plat->m_High = P_FloorHeight(sec);
 			plat->m_Status = DPlat::down;
 			plat->PlayPlatSound();
 			break;
@@ -373,8 +373,8 @@ manual_plat:
 		case DPlat::platUpWaitDownStay:
 			plat->m_High = P_FindHighestFloorSurrounding (sec);
 
-			if (plat->m_High < sec->floorheight)
-				plat->m_High = sec->floorheight;
+			if (plat->m_High < P_FloorHeight(sec))
+				plat->m_High = P_FloorHeight(sec);
 
 			plat->m_Status = DPlat::up;
 			plat->PlayPlatSound();
@@ -383,13 +383,13 @@ manual_plat:
 		case DPlat::platPerpetualRaise:
 			plat->m_Low = P_FindLowestFloorSurrounding (sec) + lip*FRACUNIT;
 
-			if (plat->m_Low > sec->floorheight)
-				plat->m_Low = sec->floorheight;
+			if (plat->m_Low > P_FloorHeight(sec))
+				plat->m_Low = P_FloorHeight(sec);
 
 			plat->m_High = P_FindHighestFloorSurrounding (sec);
 
-			if (plat->m_High < sec->floorheight)
-				plat->m_High = sec->floorheight;
+			if (plat->m_High < P_FloorHeight(sec))
+				plat->m_High = P_FloorHeight(sec);
 
 			plat->m_Status = P_Random () & 1 ? DPlat::down : DPlat::up;
 
@@ -400,25 +400,25 @@ manual_plat:
 			plat->m_Crush = false;	//jff 3/14/98 crush anything in the way
 
 			// set up toggling between ceiling, floor inclusive
-			plat->m_Low = sec->ceilingheight;
-			plat->m_High = sec->floorheight;
+			plat->m_Low = P_CeilingHeight(sec);
+			plat->m_High = P_FloorHeight(sec);
 			plat->m_Status = DPlat::down;
 // 			SN_StartSequence (sec, "Silence");
 			break;
 
 		case DPlat::platDownToNearestFloor:
-			plat->m_Low = P_FindNextLowestFloor (sec, sec->floorheight) + lip*FRACUNIT;
+			plat->m_Low = P_FindNextLowestFloor(sec) + lip*FRACUNIT;
 			plat->m_Status = DPlat::down;
-			plat->m_High = sec->floorheight;
+			plat->m_High = P_FloorHeight(sec);
 			plat->PlayPlatSound();
 			break;
 
 		case DPlat::platDownToLowestCeiling:
 		    plat->m_Low = P_FindLowestCeilingSurrounding (sec);
-			plat->m_High = sec->floorheight;
+			plat->m_High = P_FloorHeight(sec);
 
-			if (plat->m_Low > sec->floorheight)
-				plat->m_Low = sec->floorheight;
+			if (plat->m_Low > P_FloorHeight(sec))
+				plat->m_Low = P_FloorHeight(sec);
 
 			plat->m_Status = DPlat::down;
 			plat->PlayPlatSound();

common/p_snapshot.cpp
@@ -186,7 +186,9 @@ void ActorSnapshot::toActor(AActor *mo) const
 		
 			mo->ceilingz = tmceilingz;
 			mo->floorz = tmfloorz;
-				
+			mo->dropoffz = tmdropoffz;
+			mo->floorsector = tmfloorsector;	
+			
 			mo->LinkToWorld();
 		}	
 	}
@@ -216,7 +218,9 @@ void ActorSnapshot::toActor(AActor *mo) const
 	if (mFields & ACT_FLAGS2)
 		mo->flags2 = mFlags2;
 	if (mFields & ACT_FRAME)
-		mo->frame = mFrame;		
+		mo->frame = mFrame;
+	if (mFields & ACT_FLOORZ)
+		mo->floorz = mFloorZ;
 }
 
 // ============================================================================

commit a78a519320dca75f923eb658dcf721ba04c84c55

-Include a sector pointer in SectorSnapshots.  Use this when extrapolating sectors in SectorSnapshotManager::getSnapshot.  This fixes crashes with FindNextLowestFloor lifts. -Destroy moving sectors with m_Status == destroy if they already exist when calling EV_DoFloor/Plat/Door.  This should fix the problem of instant-lift floors sometimes getting stuck. -Rewrite FindNextLowestFloor, FindNextHighestFloor, FindNextLowestCeiling, and FindNextHighestCeiling.  These have been broken since r2967.
git-svn-id: http://odamex.net/svn/root/branches/0.6-dev@3141 b0bcef46-2027-0410-bd56-ab726e9a1d04

client/src/cl_main.cpp
@@ -2508,6 +2508,8 @@ void CL_UpdateMovingSector(void)
 	if (!sectors || sectornum >= numsectors)
 		return;
 
+	snap.setSector(&sectors[sectornum]);
+	
 	sector_snaps[sectornum].addSnapshot(snap);
 }
 

common/p_doors.cpp
@@ -369,6 +369,12 @@ BOOL EV_DoDoor (DDoor::EVlDoor type, line_t *line, AActor *thing,
 		sec = sides[line->sidenum[1]].sector;
 		secnum = sec-sectors;
 
+		if (sec->ceilingdata && P_MovingCeilingCompleted(sec))
+		{
+			sec->ceilingdata->Destroy();
+			sec->ceilingdata = NULL;
+		}
+		
 		// if door already has a thinker, use it
 		door = static_cast<DDoor *>(sec->ceilingdata);
 		// cph 2001/04/05 -

common/p_floor.cpp
@@ -477,7 +477,12 @@ BOOL EV_DoFloor (DFloor::EFloor floortype, line_t *line, int tag,
 manual_floor:
 		// ALREADY MOVING?	IF SO, KEEP GOING...
 		if (sec->floordata)
-			continue;
+		{
+			if (P_MovingFloorCompleted(sec))
+				sec->floordata->Destroy();
+			else
+				continue;
+		}
 
 		// new floor thinker
 		rtn = true;
@@ -786,6 +791,9 @@ int EV_DoDonut (int tag, fixed_t pillarspeed, fixed_t slimespeed)
 			floor->m_Texture = s3->floorpic;
 			floor->m_NewSpecial = 0;
 			floor->m_FloorDestHeight = P_FloorHeight(s3);
+			floor->m_Change = 0;
+			floor->m_Height = 0;
+			floor->m_Line = NULL;
 			floor->PlayFloorSound();
 
 			//	Spawn lowering donut-hole
@@ -798,6 +806,9 @@ int EV_DoDonut (int tag, fixed_t pillarspeed, fixed_t slimespeed)
 			floor->m_Sector = s1;
 			floor->m_Speed = pillarspeed;
 			floor->m_FloorDestHeight = P_FloorHeight(s3);
+			floor->m_Change = 0;
+			floor->m_Height = 0;
+			floor->m_Line = NULL;			
 			floor->PlayFloorSound();
 			break;
 		}
@@ -881,6 +892,9 @@ void DElevator::PlayElevatorSound()
 //
 void DElevator::RunThink ()
 {
+	if (m_Status == destroy)
+		return;
+		
 	EResult res;
 
 	if (m_Direction < 0)	// moving down
@@ -939,6 +953,17 @@ BOOL EV_DoElevator (line_t *line, DElevator::EElevator elevtype,
 		sec = &sectors[secnum];
 
 		// If either floor or ceiling is already activated, skip it
+		if (sec->ceilingdata && P_MovingCeilingCompleted(sec))
+		{
+			sec->ceilingdata->Destroy();
+			sec->ceilingdata = NULL;
+		}
+		if (sec->floordata && P_MovingFloorCompleted(sec))
+		{
+			sec->floordata->Destroy();
+			sec->floordata = NULL;
+		}
+		
 		if (sec->floordata || sec->ceilingdata) //jff 2/22/98
 			continue;
 		

common/p_plats.cpp
@@ -419,7 +419,12 @@ BOOL EV_DoPlat (int tag, line_t *line, DPlat::EPlatType type, fixed_t height,
 
 manual_plat:
 		if (sec->floordata)
-			continue;
+		{
+			if (P_MovingFloorCompleted(sec))
+				sec->floordata->Destroy();
+			else
+				continue;
+		}
 		
 		// Find lowest & highest floors around sector
 		rtn = true;

common/p_snapshot.cpp
@@ -611,7 +611,7 @@ void P_SetPlayerSnapshotNoPosition(player_t *player, const PlayerSnapshot &snap)
 
 SectorSnapshot::SectorSnapshot(int time) :
 	Snapshot(time),	mCeilingMoverType(SEC_INVALID), mFloorMoverType(SEC_INVALID),
-	mCeilingType(0), mFloorType(0), mCeilingTag(0), mFloorTag(0),
+	mSector(NULL), mCeilingType(0), mFloorType(0), mCeilingTag(0), mFloorTag(0),
 	mCeilingLine(NULL), mFloorLine(NULL), mCeilingHeight(0), mFloorHeight(0),
 	mCeilingSpeed(0), mFloorSpeed(0), mCeilingDestination(0), mFloorDestination(0),
 	mCeilingDirection(0), mFloorDirection(0), mCeilingOldDirection(0), mFloorOldDirection(0),
@@ -626,7 +626,8 @@ SectorSnapshot::SectorSnapshot(int time) :
 }
 
 SectorSnapshot::SectorSnapshot(int time, sector_t *sector) :
-	Snapshot(time), mCeilingMoverType(SEC_INVALID), mFloorMoverType(SEC_INVALID)
+	Snapshot(time), mCeilingMoverType(SEC_INVALID), mFloorMoverType(SEC_INVALID),
+	mSector(sector)
 {
 	if (!sector)
 	{
@@ -1048,21 +1049,22 @@ SectorSnapshot SectorSnapshotManager::getSnapshot(int time) const
 	{
 		if (mValidSnapshot(prevsnaptime))
 		{
+			const SectorSnapshot *snap = &mSnaps[prevsnaptime % NUM_SNAPSHOTS];
+			
 			// turn off any sector movement sounds from RunThink()
 			bool oldpredicting = predicting;
 			predicting = true;
-	
+		
 			// create a temporary sector for the snapshot and run the
 			// sector movement til we get to the desired time
 			sector_t tempsector;
-			memset(&tempsector, 0, sizeof(sector_t));
+			memcpy(&tempsector, snap->getSector(), sizeof(sector_t));
 			
 			// set values for the Z parameter of the sector's planes so that
 			// P_SetCeilingHeight/P_SetFloorHeight will work properly
 			tempsector.floorplane.c = tempsector.floorplane.invc = FRACUNIT;
 			tempsector.ceilingplane.c = tempsector.ceilingplane.invc = -FRACUNIT;
 						
-			const SectorSnapshot *snap = &mSnaps[prevsnaptime % NUM_SNAPSHOTS];
 			snap->toSector(&tempsector);
 
 			for (int i = 0; i < time - prevsnaptime; i++)

common/p_snapshot.h
@@ -519,6 +519,7 @@ public:
 
 	void setCeilingMoverType(movertype_t v)	{ mCeilingMoverType = v; }
 	void setFloorMoverType(movertype_t val)	{ mFloorMoverType = val; }
+	void setSector(sector_t *val)			{ mSector = val; }
 	void setCeilingType(int val)			{ mCeilingType = val; }
 	void setFloorType(int val)				{ mFloorType = val; }	
 	void setCeilingTag(int val)				{ mCeilingTag = val; }
@@ -568,6 +569,7 @@ public:
 		
 	movertype_t	getCeilingMoverType() const	{ return mCeilingMoverType; }
 	movertype_t	getFloorMoverType() const	{ return mFloorMoverType; }	
+	sector_t* getSector() const				{ return mSector; }
 	int		getCeilingType() const			{ return mCeilingType; }
 	int		getFloorType() const			{ return mFloorType; }	
 	int		getCeilingTag() const			{ return mCeilingTag; }
@@ -619,6 +621,8 @@ private:
 	movertype_t		mCeilingMoverType;
 	movertype_t		mFloorMoverType;
 
+	sector_t*		mSector;
+	
 	int				mCeilingType;
 	int				mFloorType;
 	int				mCeilingTag;

common/p_spec.cpp
@@ -718,32 +718,30 @@ fixed_t P_FindHighestFloorSurrounding (sector_t *sec)
 fixed_t P_FindNextHighestFloor (sector_t *sec)
 {
 	sector_t *other;
-	fixed_t height, heightdiff = MAXINT;
-	height = P_FloorHeight(sec->lines[0]->v1->x, sec->lines[0]->v1->y, sec);	
+	fixed_t ogheight = P_FloorHeight(sec);
+	fixed_t height = MAXINT;
 
     for (int i = 0; i < sec->linecount; i++)
     {
 		if (NULL != (other = getNextSector(sec->lines[i], sec)))
         {
-			fixed_t ofloor = P_FloorHeight(sec->lines[i]->v1->x, sec->lines[i]->v1->y, other);
-			fixed_t floor = P_FloorHeight(sec->lines[i]->v1->x, sec->lines[i]->v1->y, sec);
-			
-			if (ofloor > floor && ofloor - floor < heightdiff)
-			{
-				heightdiff = ofloor - floor;
+        	vertex_t *v = sec->lines[i]->v1;
+			fixed_t ofloor = P_FloorHeight(v->x, v->y, other);
+
+			if (ofloor < height && ofloor > ogheight)
 				height = ofloor;
-			}
 
-			ofloor = P_FloorHeight(sec->lines[i]->v2->x, sec->lines[i]->v2->y, other);
-			floor = P_FloorHeight(sec->lines[i]->v2->x, sec->lines[i]->v2->y, sec);
+			v = sec->lines[i]->v2;
+			ofloor = P_FloorHeight(v->x, v->y, other);
 
-            if (ofloor > floor && ofloor - floor < heightdiff)
-            {
-                heightdiff = ofloor - floor;
+			if (ofloor < height && ofloor > ogheight)
                 height = ofloor;
-            }
         }
     }
+    
+    if (height == MAXINT)
+    	height = ogheight;
+
     return height;
 }
 
@@ -763,32 +761,30 @@ fixed_t P_FindNextHighestFloor (sector_t *sec)
 fixed_t P_FindNextLowestFloor(sector_t *sec)
 {
 	sector_t *other;
-	fixed_t height, heightdiff = MAXINT;
-	height = P_FloorHeight(sec->lines[0]->v1->x, sec->lines[0]->v1->y, sec);	
+	fixed_t ogheight = P_FloorHeight(sec);
+	fixed_t height = MININT;
 
     for (int i = 0; i < sec->linecount; i++)
     {
 		if (NULL != (other = getNextSector(sec->lines[i], sec)))
         {
-			fixed_t ofloor = P_FloorHeight(sec->lines[i]->v1->x, sec->lines[i]->v1->y, other);
-			fixed_t floor = P_FloorHeight(sec->lines[i]->v1->x, sec->lines[i]->v1->y, sec);
-			
-			if (ofloor < floor && floor - ofloor < heightdiff)
-			{
-				heightdiff = ofloor - floor;
+        	vertex_t *v = sec->lines[i]->v1;
+			fixed_t ofloor = P_FloorHeight(v->x, v->y, other);
+
+			if (ofloor > height && ofloor < ogheight)
 				height = ofloor;
-			}
 
-			ofloor = P_FloorHeight(sec->lines[i]->v2->x, sec->lines[i]->v2->y, other);
-			floor = P_FloorHeight(sec->lines[i]->v2->x, sec->lines[i]->v2->y, sec);
+			v = sec->lines[i]->v2;
+			ofloor = P_FloorHeight(v->x, v->y, other);
 
-            if (ofloor < floor && floor - ofloor < heightdiff)
-            {
-                heightdiff = ofloor - floor;
+			if (ofloor > height && ofloor < ogheight)
                 height = ofloor;
-            }
         }
     }
+    
+    if (height == MININT)
+    	height = ogheight;
+
     return height;
 }
 
@@ -807,32 +803,30 @@ fixed_t P_FindNextLowestFloor(sector_t *sec)
 fixed_t P_FindNextLowestCeiling (sector_t *sec)
 {
 	sector_t *other;
-	fixed_t height, heightdiff = MAXINT;
-	height = P_CeilingHeight(sec->lines[0]->v1->x, sec->lines[0]->v1->y, sec);	
+	fixed_t ogheight = P_CeilingHeight(sec);
+	fixed_t height = MININT;
 
     for (int i = 0; i < sec->linecount; i++)
     {
 		if (NULL != (other = getNextSector(sec->lines[i], sec)))
         {
-			fixed_t oceil = P_CeilingHeight(sec->lines[i]->v1->x, sec->lines[i]->v1->y, other);
-			fixed_t ceil = P_CeilingHeight(sec->lines[i]->v1->x, sec->lines[i]->v1->y, sec);
-			
-			if (oceil < ceil && ceil - oceil < heightdiff)
-			{
-				heightdiff = ceil - oceil;
-				height = oceil;
-			}
+        	vertex_t *v = sec->lines[i]->v1;
+			fixed_t oceiling = P_CeilingHeight(v->x, v->y, other);
 
-			oceil = P_CeilingHeight(sec->lines[i]->v2->x, sec->lines[i]->v2->y, other);
-			ceil = P_CeilingHeight(sec->lines[i]->v2->x, sec->lines[i]->v2->y, sec);
+			if (oceiling > height && oceiling < ogheight)
+				height = oceiling;
 
-            if (oceil < ceil && ceil - oceil < heightdiff)
-            {
-                heightdiff = ceil - oceil;
-                height = oceil;
-            }
+			v = sec->lines[i]->v2;
+			oceiling = P_CeilingHeight(v->x, v->y, other);
+
+			if (oceiling > height && oceiling < ogheight)
+                height = oceiling;
         }
     }
+    
+    if (height == MININT)
+    	height = ogheight;
+
     return height;
 }
 
@@ -852,32 +846,30 @@ fixed_t P_FindNextLowestCeiling (sector_t *sec)
 fixed_t P_FindNextHighestCeiling (sector_t *sec)
 {
 	sector_t *other;
-	fixed_t height, heightdiff = MAXINT;
-	height = P_CeilingHeight(sec->lines[0]->v1->x, sec->lines[0]->v1->y, sec);
+	fixed_t ogheight = P_CeilingHeight(sec);
+	fixed_t height = MAXINT;
 
     for (int i = 0; i < sec->linecount; i++)
     {
 		if (NULL != (other = getNextSector(sec->lines[i], sec)))
         {
-			fixed_t oceil = P_CeilingHeight(sec->lines[i]->v1->x, sec->lines[i]->v1->y, other);
-			fixed_t ceil = P_CeilingHeight(sec->lines[i]->v1->x, sec->lines[i]->v1->y, sec);
-			
-			if (oceil > ceil && oceil - ceil < heightdiff)
-			{
-				heightdiff = oceil - ceil;
-				height = oceil;
-			}
+        	vertex_t *v = sec->lines[i]->v1;
+			fixed_t oceiling = P_CeilingHeight(v->x, v->y, other);
 
-			oceil = P_CeilingHeight(sec->lines[i]->v2->x, sec->lines[i]->v2->y, other);
-			ceil = P_CeilingHeight(sec->lines[i]->v2->x, sec->lines[i]->v2->y, sec);
+			if (oceiling < height && oceiling > ogheight)
+				height = oceiling;
 
-            if (oceil > ceil && oceil - ceil < heightdiff)
-            {
-                heightdiff = oceil - ceil;
-                height = oceil;
-            }
+			v = sec->lines[i]->v2;
+			oceiling = P_CeilingHeight(v->x, v->y, other);
+
+			if (oceiling < height && oceiling > ogheight)
+                height = oceiling;
         }
     }
+    
+    if (height == MAXINT)
+    	height = ogheight;
+
     return height;
 }
 