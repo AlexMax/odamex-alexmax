24310004877262ed8a234d5dbca448e6abb70c5e
AlexMax <alexmax2742@gmail.com>
2012-05-22 23:44:11 -0400

    Add "Enter" and "Hits Floor" to mobjinfo.

--- common/info.cpp
+++ common/info.cpp
@@ -6317,7 +6317,67 @@ mobjinfo_t mobjinfo[NUMMOBJTYPES] = {
 	S_NULL,		// raisestate
 	0x10000,
 	"MT_ZDOOMBRIDGE"
-	}	
+	},
+
+	{		// MT_SECACTENTER
+	9998,		// doomednum
+	S_TNT1,		// spawnstate
+	1000,		// spawnhealth
+	S_NULL,		// seestate
+	NULL,		// seesound
+	8,		// reactiontime
+	NULL,		// attacksound
+	S_NULL,		// painstate
+	0,		// painchance
+	NULL,		// painsound
+	S_NULL,		// meleestate
+	S_NULL,		// missilestate
+	S_NULL,		// deathstate
+	S_NULL,		// xdeathstate
+	NULL,		// deathsound
+	0,		// speed
+	16*FRACUNIT,		// radius
+	16*FRACUNIT,		// height
+	16*FRACUNIT,	// cdheight
+	100,		// mass
+	0,		// damage
+	NULL,		// activesound
+	0,		// flags
+	MF2_DONTDRAW,		// flags2
+	S_NULL,		// raisestate
+	0x10000,
+	"MT_SECACTENTER"
+	},
+
+	{		// MT_SECACTHITFLOOR
+	9999,		// doomednum
+	S_TNT1,		// spawnstate
+	1000,		// spawnhealth
+	S_NULL,		// seestate
+	NULL,		// seesound
+	8,		// reactiontime
+	NULL,		// attacksound
+	S_NULL,		// painstate
+	0,		// painchance
+	NULL,		// painsound
+	S_NULL,		// meleestate
+	S_NULL,		// missilestate
+	S_NULL,		// deathstate
+	S_NULL,		// xdeathstate
+	NULL,		// deathsound
+	0,		// speed
+	16*FRACUNIT,		// radius
+	16*FRACUNIT,		// height
+	16*FRACUNIT,	// cdheight
+	100,		// mass
+	0,		// damage
+	NULL,		// activesound
+	0,		// flags
+	MF2_DONTDRAW,		// flags2
+	S_NULL,		// raisestate
+	0x10000,
+	"MT_SECACTHITFLOOR"
+	}
 };
 
 VERSION_CONTROL (info_cpp, "$Id$")
