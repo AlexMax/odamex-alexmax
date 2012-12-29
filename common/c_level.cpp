AlexMax/odamex-alexmax

commit ead95f6426a9af37eb5dcabb0b4a49d438779fc7

-Remove svc_loadwad and incorporate it into svc_loadmap. -Add common/d_main.cpp, which is an initial attempt to refactor common code between client/src/d_main.cpp and server/src/d_main.cpp -Add a common G_LoadWad function, which contains the majority of the code that used to be in the console command 'wad'. -Add wad and patch file names to netdemo snapshots. -Fix skipping between maps in netdemos that have wad-switching. -Add netdemo map skipping checkpoints for map soft-resets.
git-svn-id: http://odamex.net/svn/root/trunk@3486 b0bcef46-2027-0410-bd56-ab726e9a1d04

common/c_level.cpp
@@ -493,6 +493,123 @@ void G_ParseMusInfo(void)
 	// Nothing yet...
 }
 
+//
+// G_LoadWad
+//
+// Determines if the vectors of wad & patch filenames differs from the currently
+// loaded ones and calls D_DoomWadReboot if so.
+//
+bool G_LoadWad(	const std::vector<std::string> &newwadfiles,
+				const std::vector<std::string> &newpatchfiles,
+				const std::vector<std::string> &newwadhashes,
+				const std::vector<std::string> &newpatchhashes,
+				const std::string &mapname)
+{
+	bool AddedIWAD = false;
+	bool Reboot = false;
+	size_t i, j;
+
+	// Did we pass an IWAD?
+	if (!newwadfiles.empty() && W_IsIWAD(newwadfiles[0]))
+		AddedIWAD = true;
+
+	// Check our environment, if the same WADs are used, ignore this command.
+
+	// Did we switch IWAD files?
+	if (AddedIWAD && !wadfiles.empty())
+	{
+		if (StdStringCompare(M_ExtractFileName(newwadfiles[0]), M_ExtractFileName(wadfiles[1]), true) != 0)
+			Reboot = true;
+	}
+
+	// Do the sizes of the WAD lists not match up?
+	if (!Reboot)
+	{
+		if (wadfiles.size() - 2 != newwadfiles.size() - (AddedIWAD ? 1 : 0))
+			Reboot = true;
+	}
+
+	// Do our WAD lists match up exactly?
+	if (!Reboot)
+	{
+		for (i = 2, j = (AddedIWAD ? 1 : 0); i < wadfiles.size() && j < newwadfiles.size(); i++, j++)
+		{
+			if (StdStringCompare(M_ExtractFileName(newwadfiles[j]), M_ExtractFileName(wadfiles[i]), true) != 0)
+			{
+				Reboot = true;
+				break;
+			}
+		}
+	}
+
+	// Do the sizes of the patch lists not match up?
+	if (!Reboot)
+	{
+		if (patchfiles.size() != newpatchfiles.size())
+			Reboot = true;
+	}
+
+	// Do our patchfile lists match up exactly?
+	if (!Reboot)
+	{
+		for (i = 0, j = 0; i < patchfiles.size() && j < newpatchfiles.size(); i++, j++)
+		{
+			if (StdStringCompare(M_ExtractFileName(newpatchfiles[j]), M_ExtractFileName(patchfiles[i]), true) != 0)
+			{
+				Reboot = true;
+				break;
+			}
+		}
+	}
+
+	if (Reboot)
+	{
+		std::vector<size_t> missing = D_DoomWadReboot(newwadfiles, newpatchfiles, newwadhashes, newpatchhashes);
+		if (!missing.empty())
+			return false;
+	}
+
+	unnatural_level_progression = true;
+	if (mapname.length())
+		G_DeferedInitNew((char *)mapname.c_str());
+	else
+		G_DeferedInitNew(startmap);
+
+	return true;
+}
+
+const char *ParseString2(const char *data);
+
+//
+// G_LoadWad
+//
+// Takes a space-separated string list of wad and patch names, which is parsed
+// into a vector of wad filenames and patch filenames and then calls
+// D_DoomWadReboot.
+//
+bool G_LoadWad(const std::string &str, const std::string &mapname)
+{
+	std::vector<std::string> newwadfiles;
+	std::vector<std::string> newpatchfiles;
+	std::vector<std::string> nohashes;	// intentionally empty
+
+	const char *data = str.c_str();
+
+	while ( (data = ParseString2(data)) )
+	{
+		std::string ext;
+
+		if (M_ExtractFileExtension(com_token, ext))
+		{
+			if (ext == "wad")
+				newwadfiles.push_back(com_token);
+			else if (ext == "deh" || ext == "bex")
+				newpatchfiles.push_back(com_token);		// Patch file
+		}
+	}
+
+	return G_LoadWad(newwadfiles, newpatchfiles, nohashes, nohashes, mapname);
+}
 
 BEGIN_COMMAND (map)
 {

client/src/cl_demo.cpp
@@ -38,6 +38,7 @@
 #include "version.h"
 #include "st_stuff.h"
 #include "p_mobj.h"
+#include "c_level.h"
 
 EXTERN_CVAR(sv_maxclients)
 EXTERN_CVAR(sv_maxplayers)
@@ -1141,9 +1142,26 @@ void NetDemo::writeConnectionSequence(buf_t *netbuffer)
 	MSG_WriteByte	(netbuffer, consoleplayer().id);
 	MSG_WriteByte	(netbuffer, consoleplayer().spectator);
 
-	// Server sends map name
+	// Server sends wads & map name
 	MSG_WriteMarker	(netbuffer, svc_loadmap);
-	MSG_WriteString	(netbuffer, level.mapname);
+
+	// send list of wads (skip over wadnames[0] == odamex.wad)  
+	MSG_WriteByte(netbuffer, MIN<size_t>(wadfiles.size() - 1, 255));
+	for (size_t i = 1; i < MIN<size_t>(wadfiles.size(), 256); i++)
+	{
+		MSG_WriteString(netbuffer, D_CleanseFileName(wadfiles[i], "wad").c_str());
+		MSG_WriteString(netbuffer, wadhashes[i].c_str());
+	}
+
+    // send list of DEH/BEX patches
+    MSG_WriteByte(netbuffer, MIN<size_t>(patchfiles.size(), 255));
+    for (size_t i = 0; i < MIN<size_t>(patchfiles.size(), 255); i++)
+    {
+        MSG_WriteString(netbuffer, D_CleanseFileName(patchfiles[i]).c_str());
+        MSG_WriteString(netbuffer, patchfiles[i].c_str());
+    }
+
+	MSG_WriteString(netbuffer, level.mapname);
 
 	// Server spawns the player
 	MSG_WriteMarker	(netbuffer, svc_spawnplayer);
@@ -1446,6 +1464,14 @@ void NetDemo::writeSnapshotData(byte *buf, size_t &length)
 	arc.WriteCount(vars_p - vars);
 	arc.Write(vars, vars_p - vars);
 
+	// write wad info
+	arc << (byte)(wadfiles.size() - 1);
+	for (size_t i = 1; i < wadfiles.size(); i++)
+		arc << wadfiles[i].c_str();
+	arc << (byte)patchfiles.size();
+	for (size_t i = 0; i < patchfiles.size(); i++)
+		arc << patchfiles[i].c_str();
+	// write map info
 	arc << level.mapname;
 	arc << (BYTE)(gamestate == GS_INTERMISSION);
 
@@ -1520,6 +1546,24 @@ void NetDemo::readSnapshotData(byte *buf, size_t length)
 	arc.Read(vars, len);
 	cvar_t::C_ReadCVars(&vars_p);
 
+	// read wad info
+	std::vector<std::string> newwadfiles, newpatchfiles;
+	byte numwads, numpatches;
+	std::string str;
+
+	arc >> numwads;
+	for (size_t i = 0; i < numwads; i++)
+	{
+		arc >> str;
+		newwadfiles.push_back(str);
+	}
+	arc >> numpatches;
+	for (size_t i = 0; i < numpatches; i++)
+	{
+		arc >> str;
+		newpatchfiles.push_back(str);
+	}
+
 	std::string mapname;
 	bool intermission;
 	arc >> mapname;
@@ -1550,6 +1594,9 @@ void NetDemo::readSnapshotData(byte *buf, size_t length)
 	// load a base level
 	savegamerestore = true;     // Use the player actors in the savegame
 	serverside = false;
+
+	G_LoadWad(newwadfiles, newpatchfiles);
+
 	G_InitNew(mapname.c_str());
 	displayplayer_id = consoleplayer_id = 1;
 	savegamerestore = false;

client/src/cl_main.cpp
@@ -63,6 +63,7 @@
 #include "p_pspr.h"
 #include "d_netcmd.h"
 #include "g_warmup.h"
+#include "c_level.h"
 
 #include <string>
 #include <vector>
@@ -3036,53 +3037,52 @@ void CL_ConsolePlayer(void)
 	digest = MSG_ReadString();
 }
 
-void CL_LoadWad(void)
-{
-	std::vector<std::string> wadfilenames, wadhashes, patchfilenames, patchhashes;
-
-	int wadcount = MSG_ReadByte();
-	while (wadcount--)
-	{
-		wadfilenames.push_back(MSG_ReadString());
-		wadhashes.push_back(MSG_ReadString());
-	}
-
-	int patchcount = MSG_ReadByte();
-	while (patchcount--)
-	{
-		patchfilenames.push_back(MSG_ReadString());
-		patchhashes.push_back(MSG_ReadString());
-	}
-
-	// Load the specified WAD and DEH files
-	std::vector<size_t> missing_files = 
-			D_DoomWadReboot(wadfilenames, patchfilenames, wadhashes);
-
-	// [SL] 2012-12-02 - Force the music to stop when the new map uses
-	// the same music lump name that is currently playing. Otherwise,
-	// the music from the old wad continues to play...
-	S_StopMusic();
-
-	// Reconnect to start download any missing files
-	if (!missing_files.empty())
-		CL_Reconnect();
-}
-
+//
+// CL_LoadMap
+//
+// Read wad & deh filenames and map name from the server and loads
+// the appropriate wads & map.
+//
 void CL_LoadMap(void)
 {
-	const char *mapname = MSG_ReadString ();
-
 	bool splitnetdemo = (netdemo.isRecording() && cl_splitnetdemos) || forcenetdemosplit;
 	forcenetdemosplit = false;
 	
 	if (splitnetdemo)
 		netdemo.stopRecording();
 	
-	if(gamestate == GS_DOWNLOAD)
-		return;
+	std::vector<std::string> newwadfiles, newwadhashes;
+	std::vector<std::string> newpatchfiles, newpatchhashes;
 
-	if(gamestate == GS_LEVEL)
-		G_ExitLevel (0, 0);
+	int wadcount = MSG_ReadByte();
+	while (wadcount--)
+	{
+		newwadfiles.push_back(MSG_ReadString());
+		newwadhashes.push_back(MSG_ReadString());
+	}
+
+	int patchcount = MSG_ReadByte();
+	while (patchcount--)
+	{
+		newpatchfiles.push_back(MSG_ReadString());
+		newpatchhashes.push_back(MSG_ReadString());
+	}
+
+	const char *mapname = MSG_ReadString ();
+
+	// Load the specified WAD and DEH files and change the level.
+	// if any WADs are missing, reconnect to begin downloading.
+	if (gamestate == GS_DOWNLOAD || 
+		!G_LoadWad(newwadfiles, newpatchfiles, newwadhashes, newpatchhashes))
+	{
+		CL_Reconnect();
+		return;
+	}
+
+	// [SL] 2012-12-02 - Force the music to stop when the new map uses
+	// the same music lump name that is currently playing. Otherwise,
+	// the music from the old wad continues to play...
+	S_StopMusic();
 
 	G_InitNew (mapname);
 
@@ -3139,6 +3139,10 @@ void CL_ResetMap()
 			mo->Destroy();
 		}
 	}
+
+	// write the map index to the netdemo
+	if (netdemo.isRecording() && recv_full_update)
+		netdemo.writeMapChange();
 }
 
 void CL_EndGame()
@@ -3221,7 +3225,6 @@ cmdmap cmds;
 void CL_InitCommands(void)
 {
 	cmds[svc_abort]				= &CL_EndGame;
-	cmds[svc_loadwad]			= &CL_LoadWad;
 	cmds[svc_loadmap]			= &CL_LoadMap;
 	cmds[svc_resetmap]			= &CL_ResetMap;
 	cmds[svc_playerinfo]		= &CL_PlayerInfo;

commit 144eab5db5b918721b254bbfc0eeb4918dfa1e2f

-Restore the ability to enter an IWAD name without the file extension with the 'wad' console command, broken in r3486.
git-svn-id: http://odamex.net/svn/root/trunk@3573 b0bcef46-2027-0410-bd56-ab726e9a1d04

common/c_level.cpp
@@ -594,13 +594,24 @@ bool G_LoadWad(const std::string &str, const std::string &mapname)
 
 	const char *data = str.c_str();
 
-	while ( (data = ParseString2(data)) )
+	for (size_t argv = 0; (data = ParseString2(data)); argv++)
 	{
 		std::string ext;
 
-		if (M_ExtractFileExtension(com_token, ext))
+		if (argv == 0 && W_IsIWAD(com_token))
 		{
-			if (ext == "wad")
+			// Add an IWAD
+			std::string iwad_name(com_token);
+
+			// The first argument in the string can be the name of an IWAD
+			// with the WAD extension omitted
+			M_AppendExtension(iwad_name, ".wad");
+
+			newwadfiles.push_back(iwad_name);
+		}
+		else if (M_ExtractFileExtension(com_token, ext))
+		{
+			if (ext == "wad" && !W_IsIWAD(com_token))
 				newwadfiles.push_back(com_token);
 			else if (ext == "deh" || ext == "bex")
 				newpatchfiles.push_back(com_token);		// Patch file